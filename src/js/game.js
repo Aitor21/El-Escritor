2a13e0c93407b998c684d8622a2971aae60f06d4
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2014-05-23 00:42:36 +0200

    Indented code
    The code has been indented

--- src/js/game.js
+++ src/js/game.js
@@ -28,76 +28,76 @@
         create: function () {
             var x = this.game.width / 2, y = this.game.height / 2;
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            
+
             this.bg = this.add.tileSprite(0, 0, 1280, 860, 'fondo');
             //this.bg.fixedToCamera = true;
-            
+
             this.wall1 = this.add.sprite(0,325,'',0);
             this.physics.enable(this.wall1, Phaser.Physics.ARCADE);
             this.wall1.body.immovable = true;
             this.wall1.body.setSize(375,500,0,0);
-            
+
             this.wall2 = this.add.sprite(650,325,'',0);
             this.physics.enable(this.wall2, Phaser.Physics.ARCADE);
             this.wall2.body.immovable = true;
             this.wall2.body.setSize(375,116,0,0);
-            
+
             this.wall3 = this.add.sprite(675,315,'',0);
             this.physics.enable(this.wall3, Phaser.Physics.ARCADE);
             this.wall3.body.immovable = true;
             this.wall3.body.setSize(600,220,0,0);
-            
+
             this.wall4 = this.add.sprite(0,225,'',0);
             this.physics.enable(this.wall4, Phaser.Physics.ARCADE);
             this.wall4.body.immovable = true;
             this.wall4.body.setSize(390,180,0,0);
-            
+
             this.wall5 = this.add.sprite(385,245,'',0);
             this.physics.enable(this.wall5, Phaser.Physics.ARCADE);
             this.wall5.body.immovable = true;
             this.wall5.body.setSize(100,100,0,0);
-            
+
             this.wall6 = this.add.sprite(540,245,'',0);
             this.physics.enable(this.wall6, Phaser.Physics.ARCADE);
             this.wall6.body.immovable = true;
             this.wall6.body.setSize(100,100,0,0);
-            
+
             this.wall7 = this.add.sprite(325,280,'',0);
             this.physics.enable(this.wall7, Phaser.Physics.ARCADE);
             this.wall7.body.immovable = true;
             this.wall7.body.setSize(100,100,0,0);
-            
+
             this.wall8 = this.add.sprite(568,285,'',0);
             this.physics.enable(this.wall8, Phaser.Physics.ARCADE);
             this.wall8.body.immovable = true;
             this.wall8.body.setSize(100,100,0,0);
-            
+
             this.mendigo = this.add.sprite(770,440, 'mendigo');
             this.physics.enable(this.mendigo, Phaser.Physics.ARCADE);
             this.mendigo.scale.set(0.294);
             this.mendigo.body.immovable = true;
             this.mendigo.body.setSize(280,320,-20,25);
-            
+
             this.perro = this.add.sprite(850, 500, 'perro');
             this.physics.enable(this.perro, Phaser.Physics.ARCADE);
             this.perro.scale.set(0.302);
             this.perro.body.immovable = true;
             this.perro.body.setSize(280,170,30,15);
-            
+
             this.niña = this.add.sprite(100,500, 'niña');
             this.physics.enable(this.niña, Phaser.Physics.ARCADE);
             this.niña.scale.set(0.5);
             this.niña.body.immovable = true;
             this.niña.body.setSize(431,80,60,280);
-            
+
             this.vecina1 = this.add.sprite(610, 320, 'vecina1');
             this.physics.enable(this.vecina1, Phaser.Physics.ARCADE);
             this.vecina1.scale.set(0.15);
             this.vecina1.body.immovable = true;
             this.vecina1.body.setSize(290,530,-5,15);
-          
-      //player  
-            
+
+            //player  
+
             this.player = this.add.sprite(550, 858, 'personajeEspaldas');
             this.player.animations.add('forward', [0,1,2,3,4,5],5, true);
             this.player.animations.add('still', [6],20,true);
@@ -107,7 +107,7 @@
             this.player.anchor.setTo(0.5, 0.98);
             this.player.body.setSize(280,320,0,0);
             this.player.scale.set(0.275, 0.275);
-            
+
             this.cursors = this.input.keyboard.createCursorKeys();
             this.upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);                   
             this.downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN); 
@@ -115,11 +115,11 @@
             this.mKey = this.input.keyboard.addKey(Phaser.Keyboard.M);                   
 
 
-              
+
         },
 
         update: function () {
-        
+
             console.log("Escala enX: "+this.player.scale.x+", Escala en Y: "+ this.player.scale.y);
             console.log("Velocidad de escala: "+this.vScale);
             // MOVIMIENTO DEL PERSONAJE
@@ -128,34 +128,34 @@
             if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
                 this.player.body.velocity.x = -100;
             }
-                
+
             else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
                 this.player.body.velocity.x = 100;
             }
-            
+
             else {
-            this.player.body.velocity.x = 0;
+                this.player.body.velocity.x = 0;
             }
-                
+
             if (this.input.keyboard.isDown(Phaser.Keyboard.UP))
             {
                 //this.player.y -= 2;
                 if(this.player.position.y >= 365){
                     this.player.body.velocity.y = -100;
                     if(!this.scaleStop){
-                    this.player.scale.y -= this.vScale;
-                    this.player.scale.x -= this.vScale;
-                    this.player.scale.set(this.player.scale.x,this.player.scale.y);
+                        this.player.scale.y -= this.vScale;
+                        this.player.scale.x -= this.vScale;
+                        this.player.scale.set(this.player.scale.x,this.player.scale.y);
                     }
                     this.player.animations.play('forward', 10, true);
                 }
             }
-            
-             else if(this.player.position.y <= 365){
-                 this.player.animations.play('still');
-                    this.player.body.velocity.y = 0;
-                }
-                
+
+            else if(this.player.position.y <= 365){
+                this.player.animations.play('still');
+                this.player.body.velocity.y = 0;
+            }
+
             else if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN))
             {
                 if(this.player.position.y <=856){    
@@ -168,24 +168,24 @@
                 }
             }
             else {
-            this.player.animations.play('still');
-            this.player.body.velocity.y = 0;
+                this.player.animations.play('still');
+                this.player.body.velocity.y = 0;
             }
-            
+
             //COMENTARIOS
-            
+
             //this.add.bitmapText(0,0, 'minecraftia','Jack se levanto esa mañana temprano para ir a caminar',32);
             this.text = this.add.text(0, 0, 'Jack se levanto esa mañana temprano para ir a caminar',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
             this.physics.arcade.collide(this.wall1, this.player,function (wall1, player){this.scaleStop=true;},null,this);
             this.physics.arcade.collide(this.wall2, this.player,function (wall2, player){this.scaleStop=true;},null,this);
             this.physics.arcade.collide(this.wall3, this.player,function (wall3, player){this.scaleStop=true;},null,this);
@@ -194,13 +194,13 @@
             this.physics.arcade.collide(this.wall6, this.player,function (wall6, player){this.scaleStop=true;},null,this);
             this.physics.arcade.collide(this.wall7, this.player,function (wall7, player){this.scaleStop=true;},null,this);
             this.physics.arcade.collide(this.wall8, this.player,function (wall8, player){this.scaleStop=true;},null,this);
-            
+
             this.physics.arcade.overlap(this.niña, this.player,function (niña, player)
             {
                 //this.add.bitmapText(0,30, 'minecraftia','Nunca habia visto una chica tan guapa… Se decía a si mismo',32);
                 this.text = this.add.text(0, 30, 'Nunca habia visto una chica tan guapa… Se decía a si mismo',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
                 this.text = this.add.text(0, 60, 'en su cabeza',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                
+
                 if (this.input.keyboard.isDown(Phaser.Keyboard.B))
                 {
                     this.text = this.add.text(0, 150, 'Se acerco a ella para declararsele abiertamente e invitarle a cenar',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
@@ -215,63 +215,62 @@
                 }
 
             },null,this);
-            
+
             this.physics.arcade.overlap(this.mendigo, this.player,function (mendigo, player)
-            {
-                    this.text = this.add.text(0, 30, 'Otro indigente más en las calles de la ciudad',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                if (this.input.keyboard.isDown(Phaser.Keyboard.B))
-                {
-                    this.text = this.add.text(0, 60, 'que menos podia hacer que ayudarle con el dinero que',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    this.text = this.add.text(0, 90, 'llevaba en sus bolsillos',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                }
-                else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
-                {
-                    this.text = this.add.text(0, 60, '¿Es que no tenemos ya bastantes vagos como tu? le',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    this.text = this.add.text(0, 90, 'grito al hombre mientras le agredia con una patada',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                }
+                                        {
+                                            this.text = this.add.text(0, 30, 'Otro indigente más en las calles de la ciudad',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            if (this.input.keyboard.isDown(Phaser.Keyboard.B))
+                                            {
+                                                this.text = this.add.text(0, 60, 'que menos podia hacer que ayudarle con el dinero que',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                                this.text = this.add.text(0, 90, 'llevaba en sus bolsillos',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            }
+                                            else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
+                                            {
+                                                this.text = this.add.text(0, 60, '¿Es que no tenemos ya bastantes vagos como tu? le',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                                this.text = this.add.text(0, 90, 'grito al hombre mientras le agredia con una patada',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            }
 
-            },null,this);
-            
+                                        },null,this);
+
             this.physics.arcade.overlap(this.perro, this.player,function (perro, player)
-            {
-                this.text = this.add.text(0, 30, 'Ahí estaba el perro que aguardaba tras su edificio todas las mañanas',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                if (this.input.keyboard.isDown(Phaser.Keyboard.B))
-                {
-                    this.text = this.add.text(0, 60, 'Jack acariciaba al perro mientras recordaba a su mascota',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    this.text = this.add.text(0, 90, 'de nino Bobby, hacia el que tenia un gran apego',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                        {
+                                            this.text = this.add.text(0, 30, 'Ahí estaba el perro que aguardaba tras su edificio todas las mañanas',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            if (this.input.keyboard.isDown(Phaser.Keyboard.B))
+                                            {
+                                                this.text = this.add.text(0, 60, 'Jack acariciaba al perro mientras recordaba a su mascota',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                                this.text = this.add.text(0, 90, 'de nino Bobby, hacia el que tenia un gran apego',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
 
 
-                }
-                else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
-                {
-                    this.text = this.add.text(0, 60, '“¡Largate de una vez, estoy harto de ver tus mierdas',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    this.add.bitmapText(0,185, 'minecraftia','por aquí! le chillaba al animal mientras le tiraba piedras',32);
-                    this.text = this.add.text(0, 90, 'por aquí! le chillaba al animal mientras le tiraba piedra',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                }
-            },null,this);
-            
+                                            }
+                                            else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
+                                            {
+                                                this.text = this.add.text(0, 60, '“¡Largate de una vez, estoy harto de ver tus mierdas',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                                this.text = this.add.text(0, 90, 'por aquí! le chillaba al animal mientras le tiraba piedra',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            }
+                                        },null,this);
+
             this.physics.arcade.overlap(this.vecina1, this.player,function (vecina1, player)
-            {
-                this.text = this.add.text(0, 30, 'Se topó con su vecina de abajo,',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                        {
+                                            this.text = this.add.text(0, 30, 'Se topó con su vecina de abajo,',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
 
-                if (this.input.keyboard.isDown(Phaser.Keyboard.B))
-                {
-                    this.text = this.add.text(0, 60, 'era una joven agradable cuya mirada penetraba en el',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    this.text = this.add.text(0, 90, 'inminentemente',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            if (this.input.keyboard.isDown(Phaser.Keyboard.B))
+                                            {
+                                                this.text = this.add.text(0, 60, 'era una joven agradable cuya mirada penetraba en el',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                                this.text = this.add.text(0, 90, 'inminentemente',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
 
 
-                }
-                else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
-                {
-                    this.text = this.add.text(0, 60, 'a la que mando un corte de mangas para no tener que saludarla',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                    
-                    this.text = this.add.text(0, 90, 'puesto que no estaba de humor para nadie',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
-                }
-            },null,this);
+                                            }
+                                            else if (this.input.keyboard.isDown(Phaser.Keyboard.M))
+                                            {
+                                                this.text = this.add.text(0, 60, 'a la que mando un corte de mangas para no tener que saludarla',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+
+                                                this.text = this.add.text(0, 90, 'puesto que no estaba de humor para nadie',{ font: "20pt Courier", fill: "#000000", stroke: "#119f4e", strokeThickness: 2 });
+                                            }
+                                        },null,this);
 
             //console.log(this.player.position.x);                
             //console.log(this.player.position.y);
-            
+
             /*function updateLine () {
                 if (this.line.length < this.content[this.index].length)
                 {
@@ -286,17 +285,17 @@
                 }
 
             }*/
-            
+
             if (this.player.position.y <= 365){
                 this.game.state.start('menu');
             }
 
-    },
-        
+        },
 
-        
-    render: function () {
-        /*this.game.debug.body(this.player);
+
+
+        render: function () {
+            /*this.game.debug.body(this.player);
         this.game.debug.body(this.perro);
         this.game.debug.body(this.mendigo);
         this.game.debug.body(this.vecina1);
@@ -309,17 +308,17 @@
         this.game.debug.body(this.wall6);
         this.game.debug.body(this.wall7);
         this.game.debug.body(this.wall8);*/
-    },
+        },
 
 
-    onInputDown: function () {
-        this.game.state.start('menu');
-    }
-        
+        onInputDown: function () {
+            this.game.state.start('menu');
+        }
 
-  };
 
-  window['el-escritor'] = window['el-escritor'] || {};
-  window['el-escritor'].Game = Game;
+    };
+
+    window['el-escritor'] = window['el-escritor'] || {};
+    window['el-escritor'].Game = Game;
 
 }());
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,51 +1,51 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  function Preloader() {
-    this.asset = null;
-    this.ready = false;
-  }
-
-  Preloader.prototype = {
-
-    preload: function () {
-      this.asset = this.add.sprite(320, 240, 'preloader');
-      this.asset.anchor.setTo(0.5, 0.5);
-
-      this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-      this.load.setPreloadSprite(this.asset);
-      this.load.image('fondo', 'assets/1lvl.png');
-      this.load.bitmapFont('minecraftia','assets/minecraftia.png','assets/minecraftia.xml');
-      this.load.image('bg', 'assets/fondomenu.png');
-      this.load.image('start', 'assets/enter.png');
-      this.load.image('titulo', 'assets/titulo.png');
-      this.load.spritesheet('mummy', 'assets/metalslug_mummy37x45.png', 37, 45, 18);
-      this.load.spritesheet('personajeEspaldas', 'assets/personaje.png', 561.571428571, 1761);
-      this.load.image('perro', 'assets/perro.png');
-      this.load.image('mendigo', 'assets/mendigo.png');
-      this.load.image('niña', 'assets/niña.png');
-      this.load.image('vecina1', 'assets/vecina1.png');
-        
-        
-        //this.load.physics('physicsData', 'assets/sprites.json');
-    },
-
-    create: function () {
-      this.asset.cropEnabled = false;
-    },
-
-    update: function () {
-      if (!!this.ready) {
-        this.game.state.start('menu');
-      }
-    },
-
-    onLoadComplete: function () {
-      this.ready = true;
+    function Preloader() {
+        this.asset = null;
+        this.ready = false;
     }
-  };
 
-  window['el-escritor'] = window['el-escritor'] || {};
-  window['el-escritor'].Preloader = Preloader;
+    Preloader.prototype = {
+
+        preload: function () {
+            this.asset = this.add.sprite(320, 240, 'preloader');
+            this.asset.anchor.setTo(0.5, 0.5);
+
+            this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+            this.load.setPreloadSprite(this.asset);
+            this.load.image('fondo', 'assets/1lvl.png');
+            this.load.bitmapFont('minecraftia','assets/minecraftia.png','assets/minecraftia.xml');
+            this.load.image('bg', 'assets/fondomenu.png');
+            this.load.image('start', 'assets/enter.png');
+            this.load.image('titulo', 'assets/titulo.png');
+            this.load.spritesheet('mummy', 'assets/metalslug_mummy37x45.png', 37, 45, 18);
+            this.load.spritesheet('personajeEspaldas', 'assets/personaje.png', 561.571428571, 1761);
+            this.load.image('perro', 'assets/perro.png');
+            this.load.image('mendigo', 'assets/mendigo.png');
+            this.load.image('niña', 'assets/niña.png');
+            this.load.image('vecina1', 'assets/vecina1.png');
+
+
+            //this.load.physics('physicsData', 'assets/sprites.json');
+        },
+
+        create: function () {
+            this.asset.cropEnabled = false;
+        },
+
+        update: function () {
+            if (!!this.ready) {
+                this.game.state.start('menu');
+            }
+        },
+
+        onLoadComplete: function () {
+            this.ready = true;
+        }
+    };
+
+    window['el-escritor'] = window['el-escritor'] || {};
+    window['el-escritor'].Preloader = Preloader;
 
 }());
